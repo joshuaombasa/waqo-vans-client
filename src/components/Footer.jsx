cdc4b5fa3bda65b45957f51b863fbb48678ed62f
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-09-15 10:38:54 +0300

    added footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <div className="footer--container">
+            <footer>Ⓒ 2022 #VANLIFE</footer>
+        </div>
+    )
+}
