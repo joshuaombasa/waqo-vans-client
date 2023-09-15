20f042d4b7a431c05c0e49458713c183d11168ba
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-09-15 10:30:31 +0300

    added App jsx file

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import About from './pages/About'
+import Vans from './pages/vans/Vans'
+import VanDetails from './pages/vans/VanDetails'
+import Dashboard from './pages/Host/Dashboard'
+import Income from './pages/Host/Income'
+import Reviews from './pages/Host/Reviews'
+import HostLayout from './components/HostLayout'
+import HostVans from './pages/Host/HostVans'
+import HostVanDetails from './pages/Host/HostVanDetails'
+import HostVanPricing from './pages/Host/HostVanPricing'
+import HostVanPhotos from './pages/Host/HostVanPhotos'
+import HostVanInfo from './pages/Host/HostVanInfo'
+function App() {
+
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='vans' element={<Vans />} />
+          <Route path='vans/:id' element={<VanDetails />} />
+          <Route path='host' element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path='income' element={<Income />} />
+            <Route path='reviews' element={<Reviews />} />
+            <Route path='vans' element={<HostVans />} />
+            <Route path='vans/:id' element={<HostVanDetails />}>
+              <Route index  element={<HostVanInfo />}/>
+              <Route path='pricing'   element={<HostVanPricing />}/>
+              <Route path='photos'   element={<HostVanPhotos />}/>
+            </Route>
+          </Route>
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
