375f2bdb9f015ccde93d2ef72a480af5fb55661e
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-09-15 10:38:30 +0300

    added header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom"
+
+export default function Header() {
+
+    const activeStyle = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+
+    return (
+        <header>
+            <Link to="/" className="logo">#VANLIFE</Link>
+            <nav className="nav--links--container">
+                <NavLink 
+                    to="/host" 
+                    className="nav--link"
+                    style={({isActive}) => isActive ? activeStyle : null}
+                >Host</NavLink>
+                <NavLink 
+                    to="/about" 
+                    className="nav--link"
+                    style={({isActive}) => isActive ? activeStyle : null}
+                >About</NavLink>
+                <NavLink 
+                    to="/vans" 
+                    className="nav--link"
+                    style={({isActive}) => isActive ? activeStyle : null}
+                >Vans</NavLink>
+            </nav>
+        </header>
+    )
+}
