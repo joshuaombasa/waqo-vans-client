7b1b5ab93329963b4fed3c7a879bef4f63be9294
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-09-17 15:58:10 +0300

    added van filter nav

--- src/pages/vans/Vans.jsx
+++ src/pages/vans/Vans.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 import VansPageVan from "../../components/VansPageVan";
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
+
 
 export default function Vans() {
 
     const [vansData, setVansData] = React.useState([])
+    const [serchParams, setSearchParams] = useSearchParams()
+    const typeFilter = serchParams.get("type")
+    
+    const filteredVans = typeFilter ?  vansData.filter(van => van.type === typeFilter) : vansData
 
     React.useEffect(() => {
         fetch("http://localhost:3000/api/vans")
@@ -20,15 +24,41 @@ export default function Vans() {
         return <h1>Loading...</h1>
     }
 
-    const vanElements = vansData.map(van => (
+    const vanElements = filteredVans.map(van => (
         <VansPageVan key={van.id} van={van} />
     ))
 
+    function handleSearchParams(key, value) {
+        setSearchParams(prevSearchParams => {
+            if (value === false) {
+                 prevSearchParams.delete(key)
+            } else {
+                 prevSearchParams.set(key, value)
+            }
+
+            return prevSearchParams
+        })
+    }
+
+
     return (
         <div className="vans--page">
-            
             <div className="vans--wrapper">
             <h1 className="bold--text">Explore our van options</h1>
+            <nav className="vans--type--nav">
+                <button 
+                    className="simple--vans--btn"
+                    onClick={() => handleSearchParams("type", "simple")}
+                >Simple</button>
+                <button 
+                    className="luxury--vans--btn"
+                    onClick={() => handleSearchParams("type", "luxury")}
+                >Luxury</button>
+                <button 
+                    className="rugged--vans--btn"
+                    onClick={() => handleSearchParams("type", "rugged")}
+                >Rugged</button>
+            </nav>
                 <section className="vans--page--container">
                     {vansData && vanElements}
                 </section>
