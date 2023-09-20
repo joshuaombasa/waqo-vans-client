1cb0bf2979fefdcb310a6541e3cfdee4d503484e
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-09-20 12:46:45 +0300

    updated component to use utility function for fetcing vans

--- src/pages/vans/Vans.jsx
+++ src/pages/vans/Vans.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import { useSearchParams } from "react-router-dom";
 import VansPageVan from "../../components/VansPageVan";
-
+import { getVans } from "../../api";
 
 export default function Vans() {
 
     const [vansData, setVansData] = React.useState([])
     const [serchParams, setSearchParams] = useSearchParams()
     const typeFilter = serchParams.get("type")
-    
-    const filteredVans = typeFilter ?  vansData.filter(van => van.type === typeFilter) : vansData
+
+    const filteredVans = typeFilter ? vansData.filter(van => van.type === typeFilter) : vansData
 
     React.useEffect(() => {
-        fetch("http://localhost:3000/api/vans")
-            .then(res => res.json())
-            .then(data => {
-
+        async function loadData() {
+            try {
+                const data = await getVans()
                 setVansData(data)
-            })
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        loadData()
     }, [])
 
     if (!vansData) {
@@ -31,9 +35,9 @@ export default function Vans() {
     function handleSearchParams(key, value) {
         setSearchParams(prevSearchParams => {
             if (!value) {
-                 prevSearchParams.delete(key)
+                prevSearchParams.delete(key)
             } else {
-                 prevSearchParams.set(key, value)
+                prevSearchParams.set(key, value)
             }
 
             return prevSearchParams
@@ -44,30 +48,30 @@ export default function Vans() {
     return (
         <div className="vans--page">
             <div className="vans--wrapper">
-            <h1 className="bold--text">Explore our van options</h1>
-            <nav className="vans--type--nav">
-                <button 
-                    className={`simple--vans--btn ${typeFilter === "simple" ? "simple--vans--selected" : null}`}
-                    onClick={() => handleSearchParams("type", "simple")}
-                >Simple</button>
-                <button 
-                    className={`luxury--vans--btn ${typeFilter === "luxury" && "luxury--vans--selected"}`}
-                    onClick={() => handleSearchParams("type", "luxury")}
-                >Luxury</button>
-                <button 
-                    className={`rugged--vans--btn ${typeFilter === "rugged" && "rugged--vans--selected"}`}
-                    onClick={() => handleSearchParams("type", "rugged")}
-                >Rugged</button>
-                <button 
-                    className="clear--filters--btn"
-                    onClick={() => handleSearchParams("type", null)}
-                >Clear filters</button>
-            </nav>
+                <h1 className="bold--text">Explore our van options</h1>
+                <nav className="vans--type--nav">
+                    <button
+                        className={`simple--vans--btn ${typeFilter === "simple" ? "simple--vans--selected" : null}`}
+                        onClick={() => handleSearchParams("type", "simple")}
+                    >Simple</button>
+                    <button
+                        className={`luxury--vans--btn ${typeFilter === "luxury" && "luxury--vans--selected"}`}
+                        onClick={() => handleSearchParams("type", "luxury")}
+                    >Luxury</button>
+                    <button
+                        className={`rugged--vans--btn ${typeFilter === "rugged" && "rugged--vans--selected"}`}
+                        onClick={() => handleSearchParams("type", "rugged")}
+                    >Rugged</button>
+                    <button
+                        className="clear--filters--btn"
+                        onClick={() => handleSearchParams("type", null)}
+                    >Clear filters</button>
+                </nav>
                 <section className="vans--page--container">
                     {vansData && vanElements}
                 </section>
             </div>
-           
+
         </div>
     )
 }
