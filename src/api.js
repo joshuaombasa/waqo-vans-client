bf4b41816ece96e5e1fcd771d1a715c02a67ea71
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-09-21 23:32:41 +0300

    added fetch logic for vandetails, hostvans, hostvandetails

--- src/api.js
+++ src/api.js
@@ -10,3 +10,42 @@ export async function getVans() {
     const data = await res.json()
     return data
 }
+
+export async function getVanDetails(id) {
+    const res = await fetch(`http://localhost:3000/api/vans/${id}`)
+    if (!res.ok) {
+        throw{
+            message: "Failed to fetch van details",
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+    const data = await res.json()
+    return data
+}
+
+export async function getHostVans() {
+    const res = await  fetch("http://localhost:3000/api/host/vans")
+    if (!res.ok) {
+        throw{
+            message: "Failed to fetch host vans",
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+    const data = await res.json()
+    return data
+}
+
+export async function getHostVanDetails(id) {
+    const res = await  fetch(`http://localhost:3000/api/vans/${id}`)
+    if (!res.ok) {
+        throw{
+            message: "Failed to fetch host van details",
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+    const data = await res.json()
+    return data
+}
