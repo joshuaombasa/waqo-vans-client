f3e21f50b657980baba14fe5f86eda619a86f0e6
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-09-21 21:49:14 +0300

    added login route

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import HostVanPhotos from './pages/Host/HostVanPhotos'
 import HostVanInfo from './pages/Host/HostVanInfo'
 import NotFound from './pages/NotFound'
 import Error from './pages/Error'
+import Login from './pages/Login'
 
 function App() {
 
@@ -26,6 +27,7 @@ function App() {
       <Route path='about' element={<About />} />
       <Route path='vans' element={<Vans />} loader={vansLoader} errorElement={<Error />} />
       <Route path='vans/:id' element={<VanDetails />} />
+      <Route path='login' element={<Login />} />
       <Route path='host' element={<HostLayout />}>
         <Route index element={<Dashboard />} />
         <Route path='income' element={<Income />} />
