3fa7074bd16f1ef8d6f6ce5e4de910b232c64b82
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-09-21 23:28:36 +0300

    added loader for vans,hostvans, hostvandetails, vandetails pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import Layout from './components/Layout'
 import Home from './pages/Home'
 import About from './pages/About'
 import Vans, { loader as vansLoader } from './pages/vans/Vans'
-import VanDetails from './pages/vans/VanDetails'
+import VanDetails, {loader as hostVansLoader} from './pages/vans/VanDetails'
 import Dashboard from './pages/Host/Dashboard'
 import Income from './pages/Host/Income'
 import Reviews from './pages/Host/Reviews'
 import HostLayout from './components/HostLayout'
-import HostVans from './pages/Host/HostVans'
-import HostVanDetails from './pages/Host/HostVanDetails'
+import HostVans, {loader as hostVanLoader} from './pages/Host/HostVans'
+import HostVanDetails, { loader as hostVanDetailsLoader } from './pages/Host/HostVanDetails'
 import HostVanPricing from './pages/Host/HostVanPricing'
 import HostVanPhotos from './pages/Host/HostVanPhotos'
 import HostVanInfo from './pages/Host/HostVanInfo'
@@ -26,14 +26,14 @@ function App() {
       <Route index element={<Home />} />
       <Route path='about' element={<About />} />
       <Route path='vans' element={<Vans />} loader={vansLoader} errorElement={<Error />} />
-      <Route path='vans/:id' element={<VanDetails />} />
+      <Route path='vans/:id' element={<VanDetails />} loader={hostVansLoader}/>
       <Route path='login' element={<Login />} />
       <Route path='host' element={<HostLayout />}>
         <Route index element={<Dashboard />} />
         <Route path='income' element={<Income />} />
         <Route path='reviews' element={<Reviews />} />
-        <Route path='vans' element={<HostVans />} />
-        <Route path='vans/:id' element={<HostVanDetails />}>
+        <Route path='vans' element={<HostVans />} loader={hostVanLoader}/>
+        <Route path='vans/:id' element={<HostVanDetails />} loader={hostVanDetailsLoader}>
           <Route index element={<HostVanInfo />} />
           <Route path='pricing' element={<HostVanPricing />} />
           <Route path='photos' element={<HostVanPhotos />} />
