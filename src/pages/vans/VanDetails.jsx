f3042a182f577471c4685ee3fc6d01cfb92f3c09
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-18 18:15:24 +0300

    updated component to display a fallback until its children have finished loading

--- src/pages/vans/VanDetails.jsx
+++ src/pages/vans/VanDetails.jsx
@@ -1,55 +1,58 @@
-import React from "react";
-import { useParams, Link, useLocation, useLoaderData } from "react-router-dom";
+import React, { Suspense } from "react";
+import { useParams, Link, useLocation, useLoaderData, defer, Await } from "react-router-dom";
 import { getVanDetails } from "../../api";
 
 export function loader({params}) {
-    return getVanDetails(params.id)
+    return defer({VanDetails: getVanDetails(params.id)})
 }
 
 export default function VanDetails() {
     const location = useLocation()
 
-    const [vanData, setVanData] = React.useState(null)
+    // const [vanData, setVanData] = React.useState(null)
 
-    const info = useLoaderData()
+    const dataPromise = useLoaderData()
 
     
 
     const params = useParams()
 
-    React.useEffect(() => {
-        fetch(`http://localhost:3000/api/vans/${params.id}`)
-            .then(res => res.json())
-            .then(data => {
+    // React.useEffect(() => {
+    //     fetch(`http://localhost:3000/api/vans/${params.id}`)
+    //         .then(res => res.json())
+    //         .then(data => {
 
-                setVanData(data)
-            })
-    }, [])
+    //             setVanData(data)
+    //         })
+    // }, [])
 
-    if (!vanData) {
-        return <h1>Loading...</h1>
-    }
+    // if (!vanData) {
+    //     return <h1>Loading...</h1>
+    // }
 
-    let color
-    if (vanData.type === "luxury") {
-        color = "#161616"
-    } else if (vanData.type === "simple") {
-        color = "#E17654"
-    } else if (vanData.type === "rugged") {
-        color = "#115E59"
-    }
-
-
-    const styles = {
-        backgroundColor: color
-    }
 
     const backVansFilter = location.state.search === "" ? "" : location.state.search
     const backVansType = location.state.typeFilter === null ? "all" : location.state.typeFilter
 
-    return (
-        <div className="van--details--page">
-            <Link
+    function renderDetails(vanData) {
+
+        let color
+        if (vanData.type === "luxury") {
+            color = "#161616"
+        } else if (vanData.type === "simple") {
+            color = "#E17654"
+        } else if (vanData.type === "rugged") {
+            color = "#115E59"
+        }
+    
+    
+        const styles = {
+            backgroundColor: color
+        }
+
+        return (
+            <>
+               <Link
                 to={`..?${backVansFilter}`}
                 relative="path"
                 className="back-button"
@@ -62,7 +65,17 @@ export default function VanDetails() {
                 <p>{vanData.description}</p>
                 <Link>Rent this van</Link>
             </div>
+            </>
+        )
+    }
 
+    return (
+        <div className="van--details--page">
+            <Suspense fallback={<h1>Loading...</h1>}>
+            <Await resolve={dataPromise.VanDetails}>
+                {renderDetails}
+            </Await>
+            </Suspense>
         </div>
     )
 }
