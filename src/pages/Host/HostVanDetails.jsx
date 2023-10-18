972e923ba637d2ed920bb0e53a8193ed842b5ad9
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-18 18:14:54 +0300

    updated component to display a fallback until its children have finished loading

--- src/pages/Host/HostVanDetails.jsx
+++ src/pages/Host/HostVanDetails.jsx
@@ -1,47 +1,33 @@
-import React from "react";
-import { useParams, Link, NavLink, Outlet, useLoaderData } from "react-router-dom";
+import React, { Suspense } from "react";
+import { useParams, Link, NavLink, Outlet, useLoaderData, defer, Await } from "react-router-dom";
 import { getHostVanDetails } from "../../api";
 
-export function loader({params}) {
-    console.log(params)
-    return getHostVanDetails(params.id)
+export function loader({ params }) {
+
+    return defer({ hostVanDetails: getHostVanDetails(params.id) })
 }
 
 export default function VanDetails() {
 
-    const [vanData, setVanData] = React.useState(null)
+    // const [vanData, setVanData] = React.useState(null)
 
-    const info = useLoaderData()
-    console.log(info)
+    const dataPromise = useLoaderData()
 
     const params = useParams()
 
-    React.useEffect(() => {
-        fetch(`http://localhost:3000/api/vans/${params.id}`)
-            .then(res => res.json())
-            .then(data => {
-                // console.log(data)
-                setVanData(data)
-            })
-    }, [])
+    // React.useEffect(() => {
+    //     fetch(`http://localhost:3000/api/vans/${params.id}`)
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             // console.log(data)
+    //             setVanData(data)
+    //         })
+    // }, [])
 
-    if (!vanData) {
-        return <h1>Loading...</h1>
-    }
+    // if (!vanData) {
+    //     return <h1>Loading...</h1>
+    // }
 
-    let color
-    if (vanData.type === "luxury") {
-        color = "#161616"
-    } else if (vanData.type === "simple") {
-        color = "#E17654"
-    } else if (vanData.type === "rugged") {
-        color = "#115E59"
-    }
-
-
-    const styles = {
-        backgroundColor: color
-    }
 
     const activeStyle = {
         fontWeight: "bold",
@@ -49,37 +35,64 @@ export default function VanDetails() {
         color: "#161616"
     }
 
+    function renderHostVanDetails(vanData) {
+
+        let color
+        if (vanData.type === "luxury") {
+            color = "#161616"
+        } else if (vanData.type === "simple") {
+            color = "#E17654"
+        } else if (vanData.type === "rugged") {
+            color = "#115E59"
+        }
+
+
+        const styles = {
+            backgroundColor: color
+        }
+
+        return (
+            <>
+                <Link
+                    to=".."
+                    relative="path"
+                    className="back-button"
+                >&larr; <span>Back to all vans</span></Link>
+                <div className="top--section">
+                    <img src={vanData.imageUrl} alt="" />
+                    <div className="desc--section">
+                        <span style={styles}>{vanData.type}</span>
+                        <h3>{vanData.name}</h3>
+                        <h4>${vanData.price}<span>/day</span></h4>
+                    </div>
+                </div>
+                <nav>
+                    <NavLink
+                        to="."
+                        end
+                        style={({ isActive }) => isActive ? activeStyle : null}
+                    >Details</NavLink>
+                    <NavLink
+                        to="pricing"
+                        style={({ isActive }) => isActive ? activeStyle : null}
+                    >Pricing</NavLink>
+                    <NavLink
+                        to="photos"
+                        style={({ isActive }) => isActive ? activeStyle : null}
+                    >Photos</NavLink>
+                </nav>
+                <Outlet context={{ vanData }} />
+            </>
+        )
+    }
+
     return (
         <div className="host--van--details--container">
-            <Link
-                to=".."
-                relative="path"
-                className="back-button"
-            >&larr; <span>Back to all vans</span></Link>
-            <div className="top--section">
-                <img src={vanData.imageUrl} alt="" />
-                <div className="desc--section">
-                    <span style={styles}>{vanData.type}</span>
-                    <h3>{vanData.name}</h3>
-                    <h4>${vanData.price}<span>/day</span></h4>
-                </div>
-            </div>
-            <nav>
-                <NavLink
-                    to="." 
-                    end
-                    style={({ isActive }) => isActive ? activeStyle : null}
-                >Details</NavLink>
-                <NavLink
-                    to="pricing" 
-                    style={({ isActive }) => isActive ? activeStyle : null}
-                >Pricing</NavLink>
-                <NavLink
-                    to="photos" 
-                    style={({ isActive }) => isActive ? activeStyle : null}
-                >Photos</NavLink>
-            </nav>
-            <Outlet context={{vanData}} />
+            <Suspense fallback={<h1>Loading...</h1>}>
+                <Await resolve={dataPromise.hostVanDetails}>
+                    {renderHostVanDetails}
+                </Await>
+            </Suspense>
         </div>
     )
 }
