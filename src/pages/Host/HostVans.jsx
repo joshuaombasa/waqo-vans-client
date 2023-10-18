9b778725f604fc26ff65b25adede49e62a555bea
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-18 18:15:09 +0300

    updated component to display a fallback until its children have finished loading

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,42 +1,57 @@
-import React from "react";
-import { useLoaderData } from "react-router-dom";
+import React, { Suspense } from "react";
+import { Await, defer, useLoaderData } from "react-router-dom";
 import HostVanItem from "../../components/HostVanItem";
 import { getHostVans } from "../../api";
 
 export function loader() {
-    return getHostVans()
+    return defer({hostVans : getHostVans()})
 }
 
 export default function HostVans() {
 
-    const [vansData, setVansData] = React.useState(null)
+    // const [vansData, setVansData] = React.useState(null)
 
-    const info = useLoaderData()
+    const dataPromise = useLoaderData()
 
-    React.useEffect(() => {
-        fetch("http://localhost:3000/api/host/vans")
-            .then(res => res.json())
-            .then(data => {
-               
-                setVansData(data)
-            })
-    },[])
+    // React.useEffect(() => {
+    //     fetch("http://localhost:3000/api/host/vans")
+    //         .then(res => res.json())
+    //         .then(data => {
+
+    //             setVansData(data)
+    //         })
+    // },[])
 
 
-    if (!vansData) {
-        return <h1>Loading</h1>
+    // if (!vansData) {
+    //     return <h1>Loading</h1>
+    // }
+
+
+
+    function renderHostVans(vansData) {
+
+        const hostVanElements = vansData.map(van => (
+            <HostVanItem key={van.id} van={van} />
+        ))
+
+        return (
+            <>
+                <h1 className="bold--text">Your listed vans</h1>
+                <div className="host--vans--page">
+                    {hostVanElements}
+                </div>
+            </>
+        )
     }
-
-    const hostVanElements  =vansData.map(van => (
-        <HostVanItem key={van.id} van={van} />
-    ))
 
     return (
         <div className="host--van--page">
-            <h1 className="bold--text">Your listed vans</h1>
-            <div className="host--vans--page">
-               {hostVanElements}
-            </div>
+            <Suspense>
+                <Await resolve={dataPromise.hostVans}>
+                    {renderHostVans}
+                </Await>
+            </Suspense>
         </div>
     )
 }
